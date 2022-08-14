8b9b192caa050954c1796de20e6f52f4969ecec9
Christian Antony Quero <christianantonyquero@gmail.com>
2022-08-15 02:48:27 +0800

    Create base for classes in main

--- main.py
+++ main.py
@@ -8,9 +8,29 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
+class Webcam:
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class FileSharer:
+    def __init__(self, filepath, api):
+        self.filepath = filepath
+        self.api = api
+
+    def share(self):
+        pass
+
+
 def main():
     """ Main entry point of the app """
-    print("hello world")
+    pass
 
 
 if __name__ == "__main__":
